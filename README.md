9cfdfdda3474218ee80dc3cba7710d90b07e531b
Willow-DataWitch <148896466+Willow-DataWitch@users.noreply.github.com>
2024-03-19 18:36:44 -0400

    initial commit of datedim readme and code

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# DateDimension
-Every reporting witch needs a date dimension. This one is mine.
+# Date Dimensions
+They're really useful for reporting. IYKYK.
+
+## But why this one?
+Because I made it just the way I like it.
+
+## Any other reason?
+- "AsOfDate" is configurable.
+- The fiscal date offset is configurable.
+- The attributes are developed as T-SQL persisted computed columns on a table, which makes this date dimension pretty straightforward to extend to include the date dimensional attributes your end users cannot live without.
+
+## Supported Database Engines?
+I wrote it for SQL Server 2019. It probably works for many versions of SQL Server, but I doubt the date functions are the same on other dialects of SQL. That said, you could probably adapt it pretty quickly. You could even send me pull request with your fancy postgres version.
+
+## What's it do?
+- Makes a schema: [datedim]
+- Makes a table: [datedim].[datedim]
+- Makes a config table: [datedim].[config]
+- Makes a config stored procedure: [datedim].[config_usp]
+- Makes a table maintenance stored procedure: [datedim].[datedim_maintenance_usp]
+- Makes a shortcut stored procedure: [datedim].[AsOfDateIs_usp]
+- Makes a scalar function to convert dates to SK Integers: [datedim].[DateToDateSK_ufn]
+
+## How do I join to it?
+Convert your dates with [datedim].[DateToDateSK_ufn] and join on [SK_Date]. It has a yyyyMMdd formatted integer for the SK. This is one of only a tiny handful of cases when I endorse smart-keys for your surrogate keys.
+
+## How do I use this script?
+Two options:
+
+### Just point a script somewhere
+1. Have dbatools installed.
+   - dbatools.io - go install that. It's wonderful. ```choco install dbatools -y; ```
+1. Clone this repo.
+1. Run in PowerShell
+    ```powershell
+    .\deploy-dateDim.ps1 -SQLInstance 'YourServerInstanceHere' -Database 'Your database name here';
+    ```
+
+### Just give me the SQL
+You want .\script.sql - run that on your SQL server in the database where you want these objects deployed.
+
+### Maintenance
+1. When you want "AsOfDate" to change (probably every time you load your data warehouse), run
+   - in PowerShell:
+        ```powershell
+        .\config-dateDim.ps1 -SQLInstance 'YourServerInstanceHere' -Database 'Your database name here' -AsOfDate $(Get-Date);
+        ```
+   - or in SQL:
+        ```sql
+        EXEC [datedim].[AsOfDateIs_usp] '1999-01-01'; /*<-- Update that date.*/
+        ```
+1. When you first set this up (or as needed), set the fiscaldate offset, in months, like this (6 would be a fiscal year that starts on July 1st. January 1, 2000 + 6 months = July 1, 2000. January 1, 2000 + -6 months = July 1, 1999):
+   - in PowerShell:
+        ```powershell
+        .\config-dateDim.ps1 -SQLInstance 'YourServerInstanceHere' -Database 'Your database name here' -FiscalDateOffsetInMonths 6;
+        ```
+   - or in SQL:
+        ```sql
+        EXEC [datedim].[config_usp] @FiscalDateOffsetInMonths = 6;
+        ```
+1. As needed (rarely, one hopes), set the start and end of the datedim like this (it defaults to 2000-2040):
+   - in PowerShell:
+        ```powershell
+        .\config-dateDim.ps1 -SQLInstance 'YourServerInstanceHere' -Database 'Your database name here' -Start '1999-01-01' -End '2999-01-01' ;
+        ```
+   - or in SQL:
+        ```sql
+        EXEC [datedim].[config_usp] @Start = '1999-01-01', @End = '2999-01-01'; /*<-- Update that date.*/
+        ```
+
+## I want to customize the date attributes
+```sql
+ALTER TABLE blah blah blah
+```
+BUT! If you want the config_usp to keep working, don't mess with the non-computed columns. Also, if you run .\deploy-dateDim.ps1 -Force, all your changes are getting dropped. Along with all your other configs, so, use with caution.
+
+## What's a computed column?
+https://www.google.com/search?q=sql+server+computed+column
+
+## I want a role-play dimension of this for each of my many dates
+Someday I might post my script for that.
+
+## What's SQL?
+May I interest you in my services as a consultant?
---
+++ script.sql
@@ -0,0 +1,81 @@
+CREATE SCHEMA [datedim] AUTHORIZATION [dbo];
+GO
+
+CREATE FUNCTION [datedim].[DateToDateSK_ufn] (
+    @Input VARCHAR(50)
+)
+RETURNS INT
+AS BEGIN
+DECLARE @DateSK INT;
+DECLARE @Date DATE;
+
+SET @Date = TRY_CONVERT(DATE,@Input);
+
+IF @Date IS NULL
+BEGIN
+    SET @DateSK = -1
+END
+ELSE
+BEGIN
+    IF @DATE = CAST('9999-12-31' AS DATE)
+    BEGIN
+        SET @DateSK = 99999999;
+    END
+    ELSE
+    BEGIN
+        SET @DateSK = YEAR(@Date) * 10000 + MONTH(@Date) * 100 + DAY(@Date);
+    END
+END
+
+RETURN @DateSK;
+END
+
+GO
+
+CREATE TABLE [datedim].[datedim] (
+     [SK_Date]   INT NOT NULL PRIMARY KEY
+    ,[Date_DT] DATE NULL
+    ,CONSTRAINT [datedim_datedim_Date_DT_unq] UNIQUE ([Date_DT])
+    ,CONSTRAINT [datedim_datedim_Date_Txt_unq] UNIQUE ([Date_Txt])
+    ,[AsOfDate_DT] DATE NOT NULL CONSTRAINT [datedim_datedim_AsOfDate_DT_df] DEFAULT '1999-01-01'
+    ,[FiscalDateOffsetInMonths] INT NOT NULL CONSTRAINT [datedim_datedim_FiscalDateOffsetInMonths_df] DEFAULT 0
+    
+    /*From here down, it's all computed columns, persisted when possible. Make sure they render well as text, because remember, you are using them as reporting headers.*/
+    ,[DaysBeforeAsOfDate] AS DATEDIFF(day, [Date_DT], [AsOfDate_DT]) PERSISTED
+    ,[Date_Txt] AS CONVERT(VARCHAR(50),[Date_DT],120) PERSISTED
+    ,[DayNumberOfWeek] AS DATEPART(dw, [Date_DT])
+    ,[EnglishDayNameOfWeek] AS DATENAME(WEEKDAY, [Date_DT])
+    ,[DayNumberOfMonth] AS DAY([Date_DT]) PERSISTED
+    ,[DayNumberOfYear] AS DATEPART(dy, [Date_DT]) PERSISTED
+    ,[WeekNumberOfYear] AS DATEPART(wk, [Date_DT])
+    ,[EnglishMonthName] AS DATENAME(Month, [Date_DT])
+    ,[MonthNumberOfYear] AS MONTH([Date_DT]) PERSISTED
+    ,[CalendarQuarter] AS DATEPART(q, [Date_DT]) PERSISTED
+    ,[CalendarYear] AS YEAR([Date_DT]) PERSISTED
+    ,[CalendarSemester] AS CASE WHEN MONTH([Date_DT]) < 7 THEN 1 ELSE 2 END PERSISTED
+    ,[FiscalQuarter] AS DATEPART(q, DATEADD(MONTH,[FiscalDateOffsetInMonths],[Date_DT])) PERSISTED
+    ,[FiscalYear] AS YEAR(DATEADD(MONTH,[FiscalDateOffsetInMonths],[Date_DT])) PERSISTED
+    ,[FiscalSemester] AS CASE WHEN MONTH(DATEADD(MONTH,[FiscalDateOffsetInMonths],[Date_DT])) < 7 THEN 1 ELSE 2 END PERSISTED
+    ,[CalendarYearAndMonthNumber] AS CAST( CAST(YEAR([Date_DT]) AS VARCHAR(10) ) + RIGHT('00' + CAST(MONTH([Date_DT]) AS VARCHAR(10) ) , 2) AS INT)
+
+    /*These are relative. They get recalculated when you update the [AsOfDate_DT] column.*/
+    ,[PastAndFuture] AS CASE WHEN [Date_DT] > [AsOfDate_DT] THEN 'The Past' WHEN [Date_DT] < [AsOfDate_DT] THEN 'The Future' WHEN [Date_DT] = [AsOfDate_DT] THEN 'As-Of Date' ELSE 'Unknown' END PERSISTED
+    ,[ThisMonth] AS CASE WHEN YEAR([Date_DT]) = YEAR([AsOfDate_DT]) AND MONTH([Date_DT]) = MONTH([AsOfDate_DT]) THEN 'This Month' ELSE 'Not This Month' END PERSISTED
+    ,[Prior30Days] AS CASE WHEN [Date_DT] < [AsOfDate_DT] AND [DATE_DT] >= DATEADD(DAY, -30,[AsOfDate_DT]) THEN 'Prior 30 Days' ELSE 'Not Prior 30 Days' END PERSISTED
+    ,[PriorMonth] AS CASE WHEN EOMONTH([Date_DT]) = EOMONTH(DATEADD(MONTH,-1,[AsOfDate_DT])) THEN 'Prior Month' ELSE 'Not Prior Month' END PERSISTED
+    ,[CummulativeThroughPriorMonth] AS CASE WHEN [Date_DT] <= EOMONTH(DATEADD(MONTH,-1,[AsOfDate_DT])) THEN 'Cummulative through Prior Month' ELSE 'Not Cummulative through Prior Month' END PERSISTED
+    ,[ThisFiscalYearThroughPriorMonth] AS CASE WHEN YEAR(DATEADD(MONTH,[FiscalDateOffsetInMonths],[Date_DT])) = YEAR(DATEADD(MONTH,[FiscalDateOffsetInMonths],[AsOfDate_DT])) AND [Date_DT] <= EOMONTH(DATEADD(MONTH,-1,[AsOfDate_DT])) THEN 'This Fiscal Year Through Prior Month' ELSE 'Not This Fiscal Year Through Prior Month' END PERSISTED
+    ,[ThisCalendarYearThroughPriorMonth] AS CASE WHEN YEAR([Date_DT]) = YEAR([AsOfDate_DT]) AND [Date_DT] <= EOMONTH(DATEADD(MONTH,-1,[AsOfDate_DT])) THEN 'This Calendar Year Through Prior Month' ELSE 'Not This Calendar Year Through Prior Month' END PERSISTED
+    ,[PriorMonthYearOverYear] AS CASE WHEN EOMONTH([Date_DT]) = EOMONTH(DATEADD(MONTH,-1,[AsOfDate_DT])) THEN 'Prior Month This Year' WHEN EOMONTH([Date_DT]) = EOMONTH(DATEADD(MONTH,-1,DATEADD(YEAR,-1,[AsOfDate_DT]))) THEN 'Prior Month Last Year'  ELSE 'Not Prior Month Year Over Year' END PERSISTED
+    ,[RecentFiscalYears] AS CASE WHEN YEAR(DATEADD(MONTH,[FiscalDateOffsetInMonths],[Date_DT])) = YEAR(DATEADD(MONTH,[FiscalDateOffsetInMonths],[AsOfDate_DT])) THEN 'This Fiscal Year' WHEN YEAR(DATEADD(MONTH,[FiscalDateOffsetInMonths],[Date_DT])) = YEAR(DATEADD(MONTH,[FiscalDateOffsetInMonths],DATEADD(YEAR,-1,[AsOfDate_DT]))) THEN 'Prior Fiscal Year' ELSE 'Not Recent Fiscal Year' END PERSISTED
+)
+GO
+CREATE PROCEDURE [datedim].[datedim_maintenance_unknownMember_usp] AS
+IF (SELECT COUNT(1) FROM [datedim].[datedim] WHERE [SK_Date] = -1) IS NULL
+BEGIN
+INSERT INTO [datedim].[datedim]
+SELECT
+     -1 AS [SK_Date]
+    ,NULL AS [Date_DT]
+END
+GO
